bb40131b1714de05cf83213e8e098afeed4f9532
YenHsingChen <YenHsiangChen@fih-foxconn.com>
2024-10-09 16:34:12 +0800

    add Vagrantfile for centos

---
+++ vms/centos/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["racc",["~> 1.4"]],["nokogiri",[">= 0"]],["vagrant-parallels",["= 2.4.2"]],["vagrant-qemu",["= 0.3.6"]],["vagrant-vmware-desktop",["= 3.0.4"]]],"checksum":"0a1631f6a27165e84afad9a277ec08d4657038ef222d68ee783341c67301b6a2","vagrant_version":"2.4.1"}
